ffb3a2d533d07a2a7df5ad6d93f0c93ebdcac735
Weezy <agittyturtle@gmail.com>
2015-11-05 10:55:44 -0700

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+var port = 300,
+    skill = 150,
+    i = 0,
+    scrolldelay = "";
+
 function Mail() {
     window.open('mailto:wayneericmaree@gmail.com');
+}
+
+function portScroll() {
+    i = i + 50;
+    if(i === port) {
+        i = 0;
+        window.clearInterval();
+    } else {
+        window.scrollBy(0,50); // horizontal and vertical scroll increments
+        scrolldelay = setTimeout('portScroll()',10); // scrolls every 100 milliseconds
+    }
+}
+
+function skillScroll() {
+    i = i + 50;
+    if(i === skill) {
+        i = 0;
+        window.clearInterval();
+    } else {
+        window.scrollBy(0,50); // horizontal and vertical scroll increments
+        scrolldelay = setTimeout('skillScroll()',10); // scrolls every 100 milliseconds
+    }
 }
